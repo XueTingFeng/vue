fbb612d474cc98051de24840f04d15e4bdbce017
unknown <coderxue@qq.com>
2023-04-14 21:10:05 +0800

    effect stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,10 @@
+import { extend } from '../shared'
+
 class ReactiveEffect{
     private _fn: any
+    deps = []
+    active = true
+    onStop?: () => void
 
     constructor(fn,public scheduler?){
         this._fn = fn
@@ -9,6 +14,22 @@ class ReactiveEffect{
         activeEffect = this
         return this._fn()
     }
+
+    stop(){
+        if(this.active){
+            cleanupEffect(this)
+            if(this.onStop){
+                this.onStop()
+            }
+            this.active = false
+        }  
+    }
+}
+
+function cleanupEffect(effect){
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    })
 }
 
 const targetMap = new Map()
@@ -26,7 +47,10 @@ export function track(target,key){
         depsMap.set(key, dep)
     }
 
+    if(!activeEffect) return
+
     dep.add(activeEffect)
+    activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key){
@@ -47,7 +71,20 @@ export function effect(fn,options: any = {}){
     // fn
     const _effect = new ReactiveEffect(fn, options.scheduler)
 
+    //options
+    Object.assign(_effect,options)
+    //extend
+    extend(_effect,options)
+    _effect.onStop = options.onStop
+
     _effect.run()
 
-    return _effect.run.bind(_effect)
+    const runner: any = _effect.run.bind(_effect)
+    runner.effect = _effect
+
+    return runner
+}
+
+export function stop(runner){
+    runner.effect.stop()
 }
